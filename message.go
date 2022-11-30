72df264131bb48408ef7ff9f66c0c5f1d6bd73e2
caicrs <caicrs.contact@gmail.com>
2022-11-30 08:29:39 -0300

    OrderMessage and Products struct added, little change on index.html

--- message.go
+++ message.go
@@ -5,3 +5,20 @@ type Message struct {
 	By   string `json:"by"`
 	Room string `json:"room"`
 }
+
+type OrderMessage struct {
+	id          string   `json:"id"`
+	client      string   `json:"client"`
+	marketplace string   `json:"marketplace"`
+	table       int      `json:"table"`
+	products    Products `json:"products"`
+	totalPrice  float32  `json:"totalPrice"`
+	createdAt   string   `json:"marketplace"`
+}
+
+type Products struct {
+	id          string  `json:"id"`
+	name        string  `json:"name"`
+	price       float32 `json:"price"`
+	marketplace string  `json:"marketplace"`
+}
